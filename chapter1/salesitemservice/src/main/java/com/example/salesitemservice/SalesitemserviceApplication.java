b37d8e9b830ea67e2e8c85053e35e19ac5081859
Petri Silen <petri.silen@nokia.com>
2024-03-01 10:12:59 +0200

    Update

---
+++ chapter1/salesitemservice/src/main/java/com/example/salesitemservice/SalesitemserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesitemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesitemserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesitemserviceApplication.class, args);
+	}
+
+}
---
+++ chapter1/salesitemservice/src/test/java/com/example/salesitemservice/SalesitemserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesitemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesitemserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
