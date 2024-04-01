fb7f169ccdb80e66e2f708b5bd5b05ab00abdcfd
pksilen <petri.silen@silensoft.com>
2024-04-01 17:23:43 +0300

    Update

---
+++ chapter6/apidesign/src/test/java/com/example/apidesign/ApidesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
