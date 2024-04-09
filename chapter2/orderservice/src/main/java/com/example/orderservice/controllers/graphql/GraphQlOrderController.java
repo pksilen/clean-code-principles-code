9861ebad5212172ca18f21a710da0de598a2eef5
Petri Silen <petri.silen@nokia.com>
2024-04-09 12:02:17 +0300

    Update

--- chapter2/orderservice/src/main/java/com/example/orderservice/controllers/graphql/GraphQlOrderController.java
+++ chapter2/orderservice/src/main/java/com/example/orderservice/controllers/graphql/GraphQlOrderController.java
@@ -8,7 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 
 @Controller
@@ -25,6 +31,13 @@ public class GraphQlOrderController {
     @Valid @Argument final InputOrder inputOrder
   ) {
     return orderService.createOrder(inputOrder);
+  }
+
+  @QueryMapping
+  public final Iterable<OutputOrder> ordersByUserAccountId(
+    @Argument final String userAccountId
+  ) {
+    return orderService.getOrdersByUserAccountId(userAccountId);
   }
 
   @QueryMapping
